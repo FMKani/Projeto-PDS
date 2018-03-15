1df894df4dfc58693a4ca22120c92305121e7906
Murillo <murillo_eum@hotmail.com>
2018-03-15 20:27:51 -0300

    Mudando interface de GerenciarFinancas

--- MoneyWise/src/main/java/moneywise/visao/GerenciarFinancas.java
+++ MoneyWise/src/main/java/moneywise/visao/GerenciarFinancas.java
@@ -1,27 +1,53 @@
 package moneywise.visao;
 
+import java.math.RoundingMode;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFormattedTextField;
 import javax.swing.JOptionPane;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JFormattedTextField.AbstractFormatterFactory;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.InternationalFormatter;
 import moneywise.dao.MovimentacaoDao;
 import moneywise.modelo.Movimentacao;
 import moneywise.modelo.Usuario;
 
 public class GerenciarFinancas extends javax.swing.JFrame {
-    
+
     Usuario usuario;
 
     public GerenciarFinancas(Usuario usuario) {
         this.usuario = usuario;
         initComponents();
+
         listarTudo();
+
         setVisible(true);
+
+        ListSelectionModel selectionModel = tabela.getSelectionModel();
+        selectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent lse) {
+                if (!selectionModel.isSelectionEmpty()) {
+
+                    int selectedRow = selectionModel.getMinSelectionIndex();
+                    setCampoMovimentacao(getMovimentacao(selectedRow));
+
+                }
+            }
+        });
+
     }
 
     @SuppressWarnings("unchecked")
@@ -38,6 +64,19 @@ public class GerenciarFinancas extends javax.swing.JFrame {
         btnVoltar = new javax.swing.JButton();
         btnExcluir = new javax.swing.JButton();
         btnAtualizar = new javax.swing.JButton();
+        lblCampoID = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        lblID = new javax.swing.JLabel();
+        lblData = new javax.swing.JLabel();
+        lblDescricao = new javax.swing.JLabel();
+        txtDescricao = new javax.swing.JTextField();
+        lblTipo = new javax.swing.JLabel();
+        lblCategoria = new javax.swing.JLabel();
+        lblValor = new javax.swing.JLabel();
+        txtValor = new javax.swing.JFormattedTextField();
+        txtData = new javax.swing.JFormattedTextField();
+        boxTipo = new javax.swing.JComboBox<>();
+        boxCategoria = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,14 +129,51 @@ public class GerenciarFinancas extends javax.swing.JFrame {
 
         btnAtualizar.setText("Atualizar");
 
+        lblCampoID.setText("ID:");
+
+        lblID.setText("-");
+
+        lblData.setText("Data:");
+
+        lblDescricao.setText("Descrição");
+
+        lblTipo.setText("Tipo");
+
+        lblCategoria.setText("Categoria");
+
+        lblValor.setText("Valor:");
+
+        txtValor.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(java.text.NumberFormat.getCurrencyInstance())));
+
+        try {
+            txtData.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        boxTipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Entrada", "Saída" }));
+
+        boxCategoria.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Alimentação", "Aluguel", "Água", "Luz", "Saúde", "Pessoal", "Outro" }));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblDescricao)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtDescricao))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnVoltar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnAtualizar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -108,14 +184,31 @@ public class GerenciarFinancas extends javax.swing.JFrame {
                                 .addComponent(lblDataFinal)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtDataFinal, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(btnVoltar))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnAtualizar)
+                                .addComponent(lblCampoID)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btnExcluir, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(lblID)))
+                        .addGap(41, 41, 41)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblData)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtData))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(56, 56, 56)
+                                .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblTipo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(boxTipo, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(25, 25, 25)
+                        .addComponent(lblCategoria)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(boxCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(lblValor)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtValor)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -130,13 +223,49 @@ public class GerenciarFinancas extends javax.swing.JFrame {
                     .addComponent(btnBuscar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblCampoID)
+                    .addComponent(lblID)
+                    .addComponent(lblData)
+                    .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblDescricao)
+                    .addComponent(txtDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTipo)
+                    .addComponent(lblCategoria)
+                    .addComponent(lblValor)
+                    .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(boxTipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(boxCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnVoltar)
                     .addComponent(btnExcluir)
                     .addComponent(btnAtualizar))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
+
+        txtValor.setFormatterFactory(new AbstractFormatterFactory() {
+
+            @Override
+            public AbstractFormatter getFormatter(JFormattedTextField tf) {
+                NumberFormat format = DecimalFormat.getInstance();
+                format.setMinimumFractionDigits(2);
+                format.setMaximumFractionDigits(2);
+                format.setRoundingMode(RoundingMode.HALF_UP);
+                InternationalFormatter formatter = new InternationalFormatter(format);
+                formatter.setAllowsInvalid(false);
+                formatter.setMinimum(0.0);
+                formatter.setMaximum(10000.00);
+                return formatter;
+            }
+        });
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -148,62 +277,76 @@ public class GerenciarFinancas extends javax.swing.JFrame {
     private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
         int dialogResult = JOptionPane.showConfirmDialog(null, "Deseja realmente excluir essa movimentação?",
                 "Aviso.", JOptionPane.YES_NO_OPTION);
-        
+
         if (dialogResult == JOptionPane.YES_OPTION) {
-             
+
             try {
                 MovimentacaoDao movDao = new MovimentacaoDao();
-                
+
                 int rowIndex = tabela.getSelectedRow();
                 int idColumnIndex = 0;
-                int id = (Integer)tabela.getValueAt(rowIndex, idColumnIndex);
-                
+                int id = (Integer) tabela.getValueAt(rowIndex, idColumnIndex);
+
                 movDao.remover(id);
                 movDao.close();
-                
-                listarTudo();
-                
+
             } catch (SQLException | ClassNotFoundException ex) {
                 Logger.getLogger(GerenciarFinancas.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        listarTudo();
+        limpaCampoMovimentacao();
         
+        JOptionPane.showMessageDialog(null, "Movimentação excluida.", "Concluido", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_btnExcluirActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> boxCategoria;
+    private javax.swing.JComboBox<String> boxTipo;
     private javax.swing.JButton btnAtualizar;
     private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnVoltar;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblCampoID;
+    private javax.swing.JLabel lblCategoria;
+    private javax.swing.JLabel lblData;
     private javax.swing.JLabel lblDataFinal;
     private javax.swing.JLabel lblDataInicial;
+    private javax.swing.JLabel lblDescricao;
+    private javax.swing.JLabel lblID;
+    private javax.swing.JLabel lblTipo;
+    private javax.swing.JLabel lblValor;
     private javax.swing.JTable tabela;
+    private javax.swing.JFormattedTextField txtData;
     private javax.swing.JFormattedTextField txtDataFinal;
     private javax.swing.JFormattedTextField txtDataInicial;
+    private javax.swing.JTextField txtDescricao;
+    private javax.swing.JFormattedTextField txtValor;
     // End of variables declaration//GEN-END:variables
 
-    private void listarTudo(){
+    private void listarTudo() {
         try {
             MovimentacaoDao movDao = new MovimentacaoDao();
             List<Movimentacao> movimentacoes = movDao.listarTudo(usuario);
-            
+
             listaTabela(movimentacoes);
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(GerenciarFinancas.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
-    private void listaTabela(List<Movimentacao> lista){
+
+    private void listaTabela(List<Movimentacao> lista) {
         DefaultTableModel model = (DefaultTableModel) tabela.getModel();
-        
+
         //Zera tabela para atualizar
         model.setRowCount(0);
-        
-        for(int i=0; i < lista.size(); i++){
+
+        for (int i = 0; i < lista.size(); i++) {
             Movimentacao mov = lista.get(i);
-                        
+
             model.addRow(new Object[]{
                 mov.getCod(),
                 dataToString(mov.getData()),
@@ -213,24 +356,91 @@ public class GerenciarFinancas extends javax.swing.JFrame {
                 mov.getValor()
             });
         }
-        
+
     }
-    
-    private String dataToString(Date data){
-        if(data == null){
+
+    private Movimentacao getMovimentacao(int rowIndex) {
+
+        Movimentacao mov = new Movimentacao(
+                (Integer) tabela.getValueAt(rowIndex, 0),
+                usuario.getEmail(),
+                (String) tabela.getValueAt(rowIndex, 2),
+                (Float) tabela.getValueAt(rowIndex, 5),
+                dataToDate((String) tabela.getValueAt(rowIndex, 1)),
+                (String) tabela.getValueAt(rowIndex, 3),
+                (String) tabela.getValueAt(rowIndex, 4));
+
+        return mov;
+
+    }
+
+    private void limpaCampoMovimentacao() {
+        lblID.setText("-");
+        txtData.setText("");
+        txtDescricao.setText("");
+        boxTipo.setSelectedIndex(0);
+        boxCategoria.setSelectedIndex(0);
+        txtValor.setText("0000,00");
+    }
+
+    private void setCampoMovimentacao(Movimentacao mov) {
+        lblID.setText(Integer.toString(mov.getCod()));
+        txtData.setText(dataToString(mov.getData()));
+        txtDescricao.setText(mov.getDescricao());
+        boxTipo.setSelectedItem(mov.getTipo());
+        boxCategoria.setSelectedItem(mov.getCategoria());
+        txtValor.setText(formatValor(Float.toString(mov.getValor())));
+    }
+
+    private String formatValor(String valor) {
+        valor = valor.replace(".", ",");
+        return valor;
+    }
+
+    private Date dataToDate(String dataOriginal) {
+        String campoVazio = "  /  /    ";
+        Date dataLimite = Date.valueOf("1900-01-01");
+
+        if (dataOriginal.equals(campoVazio)) {
+            return null;
+        }
+
+        DateTimeFormatter formatoOriginal = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatoNovo = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dataFormatada = formatoNovo.format(formatoOriginal.parse(dataOriginal));
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Date data = Date.valueOf(dataFormatada);
+
+        if (data.before(dataLimite)) {
+            //
+        }
+
+        return data;
+    }
+
+    private float valorToFloat(String valor) throws NumberFormatException {
+        valor = valor.replace(".", "");
+        valor = valor.replace(",", ".");
+
+        float result = Float.parseFloat(valor);
+        return result;
+    }
+
+    private String dataToString(Date data) {
+        if (data == null) {
             return "";
         }
-        
+
         LocalDate localDate = data.toLocalDate();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         return localDate.format(formatter);
     }
-    
+
     private void voltar() {
         Home home = new Home(usuario);
         home.setLocation(this.getX(), this.getY());
         this.dispose();
     }
-    
+
 }
